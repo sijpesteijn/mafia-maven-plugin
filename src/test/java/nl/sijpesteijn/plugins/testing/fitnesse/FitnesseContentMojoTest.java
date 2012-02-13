af4cd39350261f735b0af2b81dc5cdafbb0e6518
bird666 <gijs@jpoint.nl>
2012-02-13 21:41:55 +0100

    fiddling with tests

--- src/test/java/nl/sijpesteijn/plugins/testing/fitnesse/FitnesseContentMojoTest.java
+++ src/test/java/nl/sijpesteijn/plugins/testing/fitnesse/FitnesseContentMojoTest.java
@@ -5,17 +5,23 @@ import java.io.File;
 import nl.sijpesteijn.testing.fitnesse.plugins.FitnesseContentMojo;
 
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
-import org.junit.Ignore;
+import org.junit.Before;
 import org.junit.Test;
 
-@Ignore
 public class FitnesseContentMojoTest extends AbstractMojoTestCase {
 
-    @Test
-    public void test() throws Exception {
-        final File testPom = new File(getBasedir(), "src/test/resources/mafia-plugin-content.xml");
-        final FitnesseContentMojo mojo = (FitnesseContentMojo) lookupMojo("content", testPom);
-        assertNotNull(mojo);
-    }
+	@Before
+	public void setup() throws Exception {
+		super.setUp();
+	}
 
+	@Test
+	public void test() throws Exception {
+		FitnesseContentMojo mojo = new FitnesseContentMojo();
+		File testPom = new File(getBasedir(), "src/test/resources/mafia-plugin-content.xml");
+		this.configureMojo(mojo, "mafia-maven-plugin", testPom);
+		// final FitnesseContentMojo mojo = (FitnesseContentMojo)
+		// lookupMojo("content", testPom);
+		mojo.execute();
+	}
 }
