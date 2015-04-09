43dec158b4040bd2c3b7d5ad7a869e98e5765432
phauer <info@philipphauer.de>
2015-04-09 11:03:56 +0200

    surefire-report: only refer to mafiaResults folder, since the test can be part of a suite.

--- src/main/java/nl/sijpesteijn/testing/fitnesse/plugins/utils/surefirereport/SurefireReportWriter.java
+++ src/main/java/nl/sijpesteijn/testing/fitnesse/plugins/utils/surefirereport/SurefireReportWriter.java
@@ -13,82 +13,81 @@ import org.apache.maven.plugin.logging.Log;
  */
 public class SurefireReportWriter {
 
-	private Log log;
+    private Log log;
 
-	private String mafiaResultsDir;
+    private String mafiaResultsDir;
 
-	public SurefireReportWriter(Log log, String mafiaResultsDir) {
-		this.log = log;
-		this.mafiaResultsDir = mafiaResultsDir;
-	}
+    public SurefireReportWriter(Log log, String mafiaResultsDir) {
+        this.log = log;
+        this.mafiaResultsDir = mafiaResultsDir;
+    }
 
-	final static String SUREFIRE_REPORT_TEMPLATE =
-		"<testsuite errors=\"%s\" skipped=\"%s\" tests=\"%s\" time=\"%s\" failures=\"%s\" name=\"%s\">%n"
-			+ "<properties/>%n"
-			+ "<testcase classname=\"%s\" time=\"%s\" name=\"%s\">%n"
-			+ "%s%n"
-			+ "</testcase>%n"
-			+ "</testsuite>%n";
+    final static String SUREFIRE_REPORT_TEMPLATE =
+        "<testsuite errors=\"%s\" skipped=\"%s\" tests=\"%s\" time=\"%s\" failures=\"%s\" name=\"%s\">%n"
+            + "<properties/>%n"
+            + "<testcase classname=\"%s\" time=\"%s\" name=\"%s\">%n"
+            + "%s%n"
+            + "</testcase>%n"
+            + "</testsuite>%n";
 
-	final static String SUREFIRE_REPORT_ERROR_PART_TEMPLATE =
-		"<error type=\"java.lang.AssertionError\" message=\"exceptions: %s wrong: %s\"><br/>"
-			+ "See %s in workspace for more details."
-			+ "</error>";
+    final static String SUREFIRE_REPORT_ERROR_PART_TEMPLATE =
+        "<error type=\"java.lang.AssertionError\" message=\"exceptions: %s wrong: %s\"><br/>"
+            + "See %s in workspace for more details."
+            + "</error>";
 
-	public void serialize(List<TestResult> testResults, File surefireReportBaseDir) {
-		for (TestResult testResult : testResults) {
-			String targetFileName = "TEST-" + testResult.getPath() + ".xml";
-			File targetFile = new File(surefireReportBaseDir, targetFileName);
-			try {
-				serialize(testResult, targetFile);
-			} catch (TestResultException ex) {
-				log.error(ex.getMessage());
-				log.debug(ex);
-			}
-		}
-	}
+    public void serialize(List<TestResult> testResults, File surefireReportBaseDir) {
+        for (TestResult testResult : testResults) {
+            String targetFileName = "TEST-" + testResult.getPath() + ".xml";
+            File targetFile = new File(surefireReportBaseDir, targetFileName);
+            try {
+                serialize(testResult, targetFile);
+            } catch (TestResultException ex) {
+                log.error(ex.getMessage());
+                log.debug(ex);
+            }
+        }
+    }
 
-	void serialize(TestResult testResult, File targetFile) throws TestResultException {
-		String errorXmlPart = createErrorXmlPart(testResult);
-		String surefireReportContent = String.format(SUREFIRE_REPORT_TEMPLATE,
-			testResult.getExceptionCount(),
-			testResult.getIgnoredTestCount(),
-			testResult.getTotalTestCount(),
-			testResult.getRunTimeInSec(),
-			testResult.getWrongTestCount(),
-			testResult.getSuiteName(),
-			testResult.getPath(),
-			testResult.getRunTimeInSec(),
-			testResult.getTestName(),
-			errorXmlPart
-			);
-		FileWriter fileWriter = null;
-		try {
-			fileWriter = new FileWriter(targetFile);
-			IOUtils.write(surefireReportContent, fileWriter);
-			fileWriter.flush();
-		} catch (IOException e) {
-			throw fireException(targetFile, e);
-		} finally {
-			IOUtils.closeQuietly(fileWriter);
-		}
-	}
+    void serialize(TestResult testResult, File targetFile) throws TestResultException {
+        String errorXmlPart = createErrorXmlPart(testResult);
+        String surefireReportContent = String.format(SUREFIRE_REPORT_TEMPLATE,
+            testResult.getExceptionCount(),
+            testResult.getIgnoredTestCount(),
+            testResult.getTotalTestCount(),
+            testResult.getRunTimeInSec(),
+            testResult.getWrongTestCount(),
+            testResult.getSuiteName(),
+            testResult.getPath(),
+            testResult.getRunTimeInSec(),
+            testResult.getTestName(),
+            errorXmlPart
+            );
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter(targetFile);
+            IOUtils.write(surefireReportContent, fileWriter);
+            fileWriter.flush();
+        } catch (IOException e) {
+            throw fireException(targetFile, e);
+        } finally {
+            IOUtils.closeQuietly(fileWriter);
+        }
+    }
 
-	private String createErrorXmlPart(TestResult testResult) {
-		boolean noTestFailures = testResult.getExceptionCount() + testResult.getWrongTestCount() == 0;
-		if (noTestFailures) {
-			return "";
-		}
-		String pathToHtmlFile = mafiaResultsDir + "tests/" + testResult.getPath() + ".html";
-		String surefireReportContent = String.format(SUREFIRE_REPORT_ERROR_PART_TEMPLATE,
-			testResult.getExceptionCount(),
-			testResult.getWrongTestCount(),
-			pathToHtmlFile
-			);
-		return surefireReportContent;
-	}
+    private String createErrorXmlPart(TestResult testResult) {
+        boolean noTestFailures = testResult.getExceptionCount() + testResult.getWrongTestCount() == 0;
+        if (noTestFailures) {
+            return "";
+        }
+        String surefireReportContent = String.format(SUREFIRE_REPORT_ERROR_PART_TEMPLATE,
+            testResult.getExceptionCount(),
+            testResult.getWrongTestCount(),
+            mafiaResultsDir
+            );
+        return surefireReportContent;
+    }
 
-	private TestResultException fireException(File targetFile, Exception e) {
-		throw new TestResultException("Couldn't write TestResult to surefire report xml in " + targetFile, e);
-	}
+    private TestResultException fireException(File targetFile, Exception e) {
+        throw new TestResultException("Couldn't write TestResult to surefire report xml in " + targetFile, e);
+    }
 }
